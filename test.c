9b2b4f60fd0b274220b41594afe35016f2a12635
Derek <ychan3@stuy.edu>
2018-10-18 11:47:43 -0400

    stuff

--- test.c
+++ test.c
@@ -4,18 +4,18 @@
 #include "node.h"
 
 int main(){
- 	struct song_node *n0;
-	strcpy(n0->name, "billy jean");
-	strcpy(n0->artist, "mj");
-	n0->next = NULL;
+	printf("Insert a 1 at the front\n");
+	struct song_node *n0  = insert_front(0, "billy jean", "mj");
+	print_list(n0);
 
 	printf("Insert a 1 at the front\n");
-	struct song_node *n1  = insert_front(0, "song2", "boast malone");
+	struct song_node *n1  = insert_front(n0, "song2", "boast malone");
 	print_list(n1);
 
-  printf("Insert a 2 at the front\n");
-  struct song_node *ol0  = insert_order(0, "songA", "A");
-	// print_list(ordered_list0);
+	printf("Insert a 1 at the front\n");
+	struct song_node *n2  = insert_order(0, "song3", "boast malone");
+	print_list(n2);
+
 
 	return 0;
 }
